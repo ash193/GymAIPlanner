bdd9a981de93456df850dec33dc6a3481b42b145
Ashley <ash.193@live.com>
2026-09-15 00:52:21 -0400

    Setup intial Auth through Neon and made adjustments to NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
+import NavBar from "./components/layout/Navbar";
+import { NeonAuthUIProvider } from "@neondatabase/neon-js/auth/react";
+import { authClient } from "./lib/auth";
+
 function App() {
-  return <></>;
+  return (
+    <NeonAuthUIProvider authClient={authClient}>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <NavBar />
+          <main className="flex-1">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/onboarding" element={<Onboarding />} />
+              <Route path="/auth/:pathname" element={<Auth />} />
+              <Route path="/account/:pathname" element={<Account />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </NeonAuthUIProvider>
+  );
 }
 
 export default App;
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,3 +1,11 @@
+import { AuthView } from "@neondatabase/neon-js/auth/react";
+import { useParams } from "react-router-dom";
+
 export default function Auth() {
-  return <div>Auth</div>;
+  const { pathname } = useParams();
+  return (
+    <div>
+      <AuthView pathname={pathname} />
+    </div>
+  );
 }
